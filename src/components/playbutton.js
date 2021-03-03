b5ae8ebbe7f1af18699932a8d669293792a528e9
tuvaelises <tuva.akerlund@hyperisland.se>
2021-03-03 15:22:18 +0100

    Fixed play-button

---
+++ src/components/playbutton.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./landing.css"
+import Play from "./Playbutton.svg"
+
+export default function playbutton(){
+    return(
+        <div className="PlayButton">
+            <img src={Play} alt="Play button" className="PlaySymbol"></img>
+            
+            </div>
+
+    )
+}
