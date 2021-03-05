843003593ae02026e7856cc35f566cf4e0973e22
wikidbrit <pflemingonline@gmail.com>
2021-03-05 09:29:12 +0100

    passing the data

--- src/components/heroButton.js
+++ src/components/heroButton.js
@@ -3,14 +3,19 @@ import './landing.css';
 import plus from './plus.svg';
 
 
-const heroButton = () => {
+const heroButton = (props) => {
+
+console.log(props.buttonData)
 
     return (
         <div className="HeroButton">
-            
+            <p>{props.buttonData.categoryName}</p>
             <img className="plusIcon" src={plus} alt="plus icon"/>
         </div>
     );
 }
 
 export default heroButton;
+
+//if you want to change the state you need to pass in functions
+//on click functions -> button expands
