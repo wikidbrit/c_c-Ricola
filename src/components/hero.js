58c86ac836d63a3df6ec0dae5888ece8ae7165ed
Lisa Lee <lisaleekallman@gmail.com>
2021-03-11 19:51:00 +0100

    fixed the hover state on the world elements

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,11 +13,29 @@ import Village from '../assets/village.png';
 import Toon from '../assets/RicolaAvatarPlaceholder.png';
 import ReactPlayer from 'react-player';
 
-function Hero({buttonData, hide, setHide, displayCardsSection, displayUnderConstruction}) {
+function Hero({buttonData, displayCardsSection, displayUnderConstruction}) {
 
     let [opacity, setOpacity] = useState("0%")
     let [display, setDisplay] = useState( "none" )
     let [playing, setPlaying] = useState( false )
+
+    let [toggleNature, setToggleNature] = useState( "+" )
+    let [toggleBusiness, setToggleBusiness] = useState( "+" )
+    let [togglePeople, setTogglePeople] = useState( "+" )
+    let [toggleXRicola, setToggleXRicola] = useState( "+" )
+
+    const hoverMountain = () => {
+        setToggleNature(prev => !prev);
+    }
+    const hoverFarm = () => {
+        setToggleBusiness(prev => !prev);
+    }
+    const hoverVillage = () => {
+        setTogglePeople(prev => !prev);
+    }
+    const hoverFactory = () => {
+        setToggleXRicola(prev => !prev);
+    }
 
     const videoToggler = () => {
         setOpacity(prev => !prev)
@@ -34,15 +52,24 @@ function Hero({buttonData, hide, setHide, displayCardsSection, displayUnderConst
                      category={item.name}
                     displayCardsSection={displayCardsSection}
                     displayUnderConstruction={displayUnderConstruction}
+                    toggleNature={toggleNature}
+                    toggleBusiness={toggleBusiness}
+                    togglePeople={togglePeople}
+                    toggleXRicola={toggleXRicola}
+                    hoverMountain={hoverMountain}
+                    hoverFarm={hoverFarm}
+                    hoverVillage={hoverVillage}
+                    hoverFactory={hoverFactory}
+
                     />
                 ))}
                 <img src={Ground} alt="Ricola world ground" className='ground' style={{zIndex:'2'}}/>
                 <img src={Toon} style={{zIndex:'2'}} alt="" className='rasmus' onClick={videoToggler} />
-                <img src={Village} style={{zIndex:'2'}} alt="A rending of a small town" className='village' onClick={displayUnderConstruction}/>
-                <img src={Farm} style={{zIndex:'2'}} alt="A rendering of a farm" className='farm' onClick={displayUnderConstruction}/>
-                <img src={Factory} style={{zIndex:'2'}} alt="A rendering of the Ricola factory" className='factory' onClick={displayUnderConstruction}/>
+                <img src={Village} style={{zIndex:'2'}} alt="A rending of a small town" className='village' onClick={displayUnderConstruction} onMouseEnter={hoverVillage} onMouseLeave={hoverVillage}/>
+                <img src={Farm} style={{zIndex:'2'}} alt="A rendering of a farm" className='farm' onClick={displayUnderConstruction} onMouseEnter={hoverFarm} onMouseLeave={hoverFarm}/>
+                <img src={Factory} style={{zIndex:'2'}} alt="A rendering of the Ricola factory" className='factory' onClick={displayUnderConstruction} onMouseEnter={hoverFactory} onMouseLeave={hoverFactory}/>
                 <img src={Mountain} style={{zIndex:'2'}} alt="Ricola world Mountain, its so impressive" className='Mountain'
-                    onClick={displayCardsSection}/>
+                    onClick={displayCardsSection} onMouseEnter={hoverMountain} onMouseLeave={hoverMountain}/>
                 <img src={RightCloud} alt="A cloud in the sky" className="RightCloud"/>
                 <img src={LeftCloudBig} alt="A cloud in the sky" className="LeftCloudBig"/>
                 <img src={LeftCloudSmall} alt="A cloud in the sky" className="LeftCloudSmall"/>       
--- src/components/heroButton.js
+++ src/components/heroButton.js
@@ -2,29 +2,39 @@ import React, { useState } from 'react';
 import './landing.css';
 
 
-const HeroButton = ({category, displayCardsSection, displayUnderConstruction}) => {
+const HeroButton = ({category, displayCardsSection, displayUnderConstruction, toggleNature, toggleBusiness, togglePeople, toggleXRicola, hoverMountain, hoverFarm, hoverVillage, hoverFactory}) => {
 
 
-    let [toggle, setToggle] = useState("+");
-    // let [toggle2, setToggle2] = useState('hide');
+    // let [toggle, setToggle] = useState("+");
+    // // let [toggle2, setToggle2] = useState('hide');
 
-    const toggler = () => {
-        setToggle(prev => !prev)
-        // setToggle2(prev => !prev)
-    }
+    // const toggler = () => {
+    //     setToggle(prev => !prev)
+    //     // setToggle2(prev => !prev)
+    // }
 
     if(category==="Nature"){
-        console.log("nature")
         return (
-            <div className={`HeroButton ${category}`} id={category} onMouseEnter={toggler} onMouseLeave={toggler} onClick={displayCardsSection}>
-                <span >{toggle ? "+" : `${category}`}</span>
+            <div className={`HeroButton ${category}`} id={category} onMouseEnter={hoverMountain} onMouseLeave={hoverMountain} onClick={displayCardsSection}>
+                <span >{toggleNature ? "+" : `${category}`}</span>
+            </div>
+        );
+    } if(category==="Business"){
+        return (
+            <div className={`HeroButton ${category}`} id={category} onMouseEnter={hoverFarm} onMouseLeave={hoverFarm} onClick={displayCardsSection}>
+                <span >{toggleBusiness ? "+" : `${category}`}</span>
+            </div>
+        );
+    } if(category==="People"){
+        return (
+            <div className={`HeroButton ${category}`} id={category} onMouseEnter={hoverVillage} onMouseLeave={hoverVillage} onClick={displayCardsSection}>
+                <span >{togglePeople ? "+" : `${category}`}</span>
             </div>
         );
     } else {
-        console.log("other")
         return (
-            <div className={`HeroButton ${category}`} id={category} onMouseEnter={toggler} onMouseLeave={toggler} onClick={displayUnderConstruction}>
-                <span >{toggle ? "+" : `${category}`}</span>
+            <div className={`HeroButton ${category}`} id={category} onMouseEnter={hoverFactory} onMouseLeave={hoverFactory} onClick={displayUnderConstruction}>
+                <span >{toggleXRicola ? "+" : `${category}`}</span>
             </div>
         );
     }
