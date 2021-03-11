5828854c2cdffe188456a1485ee9269a68d45199
Lisa Lee <lisaleekallman@gmail.com>
2021-03-11 16:59:00 +0100

    removed the menu

--- src/components/header.js
+++ src/components/header.js
@@ -13,26 +13,6 @@ export default function Header() {
     
         <div className="headerDiv" style={{"position":"fixed", "top": "0", 'backgroundColor':'white'}}>
             <img src={ricolaLogo} className="ricolaLogo" alt="Ricola Logo"/>
-            <nav>
-                <a href="" className="menu-toggle">
-                    <div className={`menu-icon ${menu ? "close" : "open"}`} onClick={toggler}>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                </a>
-                {/* <img src={hamburger} className="hamburgerIcon" alt="Dropdown Menu Icon"/> */}
-            </nav>
-            {/* <div className="menu">
-                <nav className="menu-nav">
-                <a href="a"i className="selected">home</a>
-                <a href="a">Nature</a>
-                <a href="a">People</a>
-                <a href="a">X-Ricola</a>
-                <a href="a">Business</a>
-                </nav> 
-            </div> */}
         </div>
         
     )
